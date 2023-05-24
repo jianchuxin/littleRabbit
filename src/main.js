f4599a5e2d6c82554f20dd270a990019a89a5b81
jianchuxin <903243301@qq.com>
2023-05-24 23:33:26 +0800

    axios封装

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
 import App from "./App.vue";
+
+import { getCategoryAPI } from "@/apis/httpTest";
+getCategoryAPI().then((res) => {
+  console.log(res);
+});
 
 const app = createApp(App);
 
