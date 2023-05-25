5d1893859aab33cc53a806a1a7982a753e4c060d
jianchuxin <903243301@qq.com>
2023-05-25 15:06:03 +0800

    静态资源引入

--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+
 import App from "./App.vue";
+import router from "@/router/index";
 
-import { getCategoryAPI } from "@/apis/httpTest";
-getCategoryAPI().then((res) => {
-  console.log(res);
-});
-
+import "@/styles/common.scss";
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(router);
 
 app.mount("#app");
---
+++ src/views/Home/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>哦是home</div>
+</template>
